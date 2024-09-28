dad348106f5c04c8bd732fff69d1b39ff0a4162c
Md Likhon Mia <89283233+YSISLM104926@users.noreply.github.com>
2024-09-28 12:52:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,38 +4,33 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const conn = require('./config/db')
 const userRoutes = require('./routes/user.routes')
 const crisisRoutes = require('./routes/crisis.routes')
 const donationRoutes = require('./routes/donation.routes')
 const inventoryRoutes = require('./routes/inventory.routes')
 const volunteerRoutes = require('./routes/volunteer.routes')
-
 const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// DB connection
+// DB Connection
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
-
-// Auth(Resgister and Login)
+// Routes
 app.use(userRoutes);
 app.use(crisisRoutes);
 app.use(donationRoutes);
 app.use(inventoryRoutes);
 app.use(volunteerRoutes);
 
-
-
 app.get('/', async (req, res) => {
   res.send('Server is running');
 })
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
